7f9c6cd7b549d69cf120117e018bcfa25dc4bdeb
bhavye2001 <72085250+bhavye2001@users.noreply.github.com>
2023-05-10 07:23:33 +0530

    Add files via upload

---
+++ Demo1/src/main/java/com/bhavye/Demo1/App.java
@@ -0,0 +1,14 @@
+package com.bhavye.Demo1;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        System.out.println("Welcome to My World");
+    }
+}
